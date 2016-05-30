da35df60add1a4bff2e906a5f0e073ffea128d0e
LoranceChen <wangchen@qiaobutang.com>
2016-05-30 20:55:41 +0800

    init

---
+++ crashBugEg/Assets/testFlatMap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Lorance.RxScoket.Util;
+
+public class testFlatMap : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		Dog d = new Dog ();
+		IOption<Dog> dogOpt = new Some<Dog> (d);
+
+		//ok
+		Dog dog = dogOpt.GetOrElse<Dog> (() => new Dog());
+
+		//crash at this point. does it care about polymorphism? but it works on pure .net project
+		Animal animal = dogOpt.GetOrElse<Animal> (() => new Cat());
+
+		print ("r2 -" + animal);
+	}
+}
+
+public class Animal {}
+public class Dog: Animal {}
+public class Cat: Animal {}
